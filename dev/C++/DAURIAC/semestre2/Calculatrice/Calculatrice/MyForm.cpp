bb7d8590fe5ce8dffb704f6d6da440655c2245aa
owhn <irqven@gmail.com>
2025-03-16 18:14:56 +0100

    Modif ajt calculatrice

---
+++ dev/C++/DAURIAC/semestre2/Calculatrice/Calculatrice/MyForm.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ConvertirTemp::MyForm form; // MonProjet est le nom de mon projet (à adapter)
+	Application::Run(% form);
+}
